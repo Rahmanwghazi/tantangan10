925e79ae0bb3869c8e3ce35a65b4d3bb91d11629
M. Rahman Wafiq Ghazi <49003404+Rahmanwghazi@users.noreply.github.com>
2019-04-08 17:57:54 +0800

    Update bubblesortdsc.cpp
    Penambahan tanda kurung

--- bubblesortdsc.cpp
+++ bubblesortdsc.cpp
@@ -27,7 +27,7 @@ void printArray (int arr[], int size)
 int main()
 {
    int arr[] = {64, 34, 25, 12, 22, 11, 90};
-   int n = sizeof(arr)/sizeof(arr[0];
+   int n = sizeof(arr)/sizeof(arr[0]);
    bubbleSort(arr, n);
    printf("Sorted array : \n");
    printArray(arr, n);
